2c06f88ed253a42c393eee1191431895b1a23f15
eylulalbayrak44 <alb.eylul44@gmail.com>
2024-10-19 13:05:12 +0300

    Add files via upload

---
+++ EylülAlbayrak_ödev.cs
@@ -0,0 +1,65 @@
+﻿//Console.WriteLine("1. Sayıyı Giriniz");
+//int Sayi_1= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2. Sayıyı Giriniz");
+//int Sayi_2 = Convert.ToInt32(Console.ReadLine());
+//int sonuc = Sayi_1 - Sayi_2;
+//Console.WriteLine("Fark Değeriniz: " + sonuc);
+
+//2.soru
+//int d = 5;
+
+//int sonuc=5*5;
+//Console.WriteLine("beşin karesi:"+ sonuc);
+
+//3. soru
+//int e = 10;
+//int f=3;
+
+//int sonuc = 10/3;
+//Console.WriteLine("Kalan Değeriniz:"+ sonuc);
+
+
+//4.soru
+//Console.WriteLine("1. Sayıyı Giriniz");
+//int Sayi_1= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2. Sayıyı Giriniz");
+//int Sayi_2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("3. Sayıyı Giriniz");
+//int Sayi_3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("4. Sayıyı Giriniz");
+//int Sayi_4 = Convert.ToInt32(Console.ReadLine());
+//int sonuc= Sayi_1 * Sayi_2 * Sayi_3 * Sayi_4;
+//Console.WriteLine("SONUÇ:"+ sonuc);
+
+
+//Console.WriteLine("1. Sayıyı Giriniz");
+//int Sayi_1= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2. Sayıyı Giriniz");
+//int Sayi_2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("3. Sayıyı Giriniz");
+//int Sayi_3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("4. Sayıyı Giriniz");
+//int Sayi_4 = Convert.ToInt32(Console.ReadLine());
+//int sonuc= Sayi_1 + Sayi_2 + Sayi_3 + Sayi_4;
+//Console.WriteLine("SONUÇ:"+ sonuc);
+
+
+//5. soru
+//Console.WriteLine("1. Sayıyı Giriniz");
+//int Sayi_1= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2. Sayıyı Giriniz");
+//int Sayi_2 = Convert.ToInt32(Console.ReadLine());
+//int sonuc = Sayi_1 / Sayi_2;
+//Console.WriteLine("SONUÇ:"+ sonuc);
+
+
+//6. soru
+//Console.WriteLine("1. Kenarı Giriniz");
+//int Kenar1= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2. Kenarı Giriniz");
+//int Kenar2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("3. Kenarı Giriniz");
+//int Kenar3 = Convert.ToInt32(Console.ReadLine());
+//int DikdörtgenlerPrizmaHacmi= Kenar1 * Kenar2 * Kenar3;
+//Console.WriteLine("Dikdörtgenler Prizmasının Hacmi :"+ DikdörtgenlerPrizmaHacmi);
+
